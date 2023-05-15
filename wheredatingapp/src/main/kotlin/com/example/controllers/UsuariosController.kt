2701f7ee50d7f35b52925ef14638aac87b5070db
agonben23 <agonzalezb@iesrafaelalberti.es>
2023-05-15 19:34:35 +0200

    arreglado

--- wheredatingapp/src/main/kotlin/com/example/controllers/UsuariosController.kt
+++ wheredatingapp/src/main/kotlin/com/example/controllers/UsuariosController.kt
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,7 +23,7 @@ class UsuariosController() {
     @Autowired
     lateinit var usuariosService: UsuariosService
 
-    @GetMapping("/")
+    @PutMapping("/")
     fun getOne(@RequestBody usuario: Usuario): ResponseEntity<Usuario?> {
 
         return try {
